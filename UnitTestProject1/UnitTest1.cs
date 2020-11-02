67b904198d1a02d77e3ac237198a3c7bef090d6e
Mislav <mislav.franja@gmail.com>
2020-11-02 18:44:54 +0100

    popravio

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Vsite.Pood.TeamCityTest
+{
+	[TestClass]
+	public class TestSquared
+	{
+		[TestMethod]
+		public void SquaredReturns4forArgument2()
+		{
+			Assert.AreEqual(4, MyMath.Squared(2));
+		}
+
+		[TestMethod]
+		public void SquaredReturns0forArgument0()
+		{
+			Assert.AreEqual(0, MyMath.Squared(0));
+		}
+
+		[TestMethod]
+		public void SquaredReturns4forArgumentMinus2()
+		{
+			Assert.AreEqual(4, MyMath.Squared(-2));
+		}
+	}
+}
+
---
+++ consolApp/Squerd.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Pood.TeamCityTest
+{
+	public static class MyMath
+	{
+		public static double Squared(double x)
+		{
+			return x * x;
+		}
+	}
+}
